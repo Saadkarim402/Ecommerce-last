b366d1028d9d60b6107372900381b304b2eff825
saadkarim402 <saadkarim782@gmail.com>
2023-07-20 11:39:36 -0700

    api testing 2

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,19 +1,29 @@
 import express from "express";
 import {
-    registerController,
-    // loginController,
-    // testController,
-    // forgotPasswordController,
-    // updateProfileController,
-    // getOrdersController,
-    // getAllOrdersController,
-    // orderStatusController,
-  } from "../controllers/authController.js";
+  registerController,
+  loginController,
+  testController,
+  // forgotPasswordController,
+  // updateProfileController,
+  // getOrdersController,
+  // getAllOrdersController,
+  // orderStatusController,
+} from "../controllers/authController.js";
+
+import { isAdmin } from "../middlewares/authMiddleware.js";
 
 //router object
 const router = express.Router();
 
 //routing
+
 //REGISTER || METHOD POST
 router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+//test routes
+router.get("/test", isAdmin, testController);
+
 export default router;
